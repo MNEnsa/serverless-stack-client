624b042168fbe7eba282554d17421ce0258a212c
Nebojsa <nebojsa_pg@hotmail.com>
2020-01-31 11:48:57 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-central-1",
+      BUCKET: "bucketforserverlessstack"
+    },
+    apiGateway: {
+      REGION: "eu-central-1",
+      URL: "https://4hj5unjs3c.execute-api.eu-central-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-central-1",
+      USER_POOL_ID: "eu-central-1_83LFkljOc",
+      APP_CLIENT_ID: "1jt60p5fj9fd8baesfkmpqpov3",
+      IDENTITY_POOL_ID: "eu-central-1:14b02006-dd89-484e-ad50-3f0715922a76"
+    }
+  };
