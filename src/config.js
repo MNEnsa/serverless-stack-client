64e530888c52ccc2f773c90116e47a576b3ea1e2
Nebojsa <nebojsa_pg@hotmail.com>
2020-02-03 12:12:04 +0100

    Building our React App

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
-    s3: {
-      REGION: "eu-central-1",
-      BUCKET: "bucketforserverlessstack"
-    },
-    apiGateway: {
-      REGION: "eu-central-1",
-      URL: "https://4hj5unjs3c.execute-api.eu-central-1.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "eu-central-1",
-      USER_POOL_ID: "eu-central-1_83LFkljOc",
-      APP_CLIENT_ID: "1jt60p5fj9fd8baesfkmpqpov3",
-      IDENTITY_POOL_ID: "eu-central-1:14b02006-dd89-484e-ad50-3f0715922a76"
-    }
-  };
+  STRIPE_KEY: "pk_test_qv7GjF8coVLgKk3MU91HvCQp00FXm2eBo",
+  MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "bucketforserverlessstack"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://4hj5unjs3c.execute-api.eu-central-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_83LFkljOc",
+    APP_CLIENT_ID: "1jt60p5fj9fd8baesfkmpqpov3",
+    IDENTITY_POOL_ID: "eu-central-1:14b02006-dd89-484e-ad50-3f0715922a76"
+  }  
+};
